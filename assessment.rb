d7da5dd3b3d69aa116c837d483eeb234a8abdcf6
Nikolay Antonov <nantonov@git.mirabeau.nl>
2019-03-29 13:22:59 +0100

    reached ruby-generated output parity with original json

--- assessment.rb
+++ assessment.rb
@@ -1,6 +1,8 @@
 require 'json'
 
 instance_type="t2.nano"
+allowed_ssh = "35.158.238.101/32"
+latest_ami_id = "ami-08935252a36e25f85"
 
 tempHash = {
     "AWSTemplateFormatVersion" => "2010-09-09",
@@ -9,8 +11,7 @@ tempHash = {
       "Keyname" => {"Description" => "Name of an existing EC2 KeyPair to enable SSH access to the instance",
                     "Type" => "AWS::EC2::KeyPair::KeyName",
                     "ConstraintDescription" => "must be the name of an existing EC2 keypair"},
-      "SSHLocation" => {"Description" => "The IP address range..."},
-    },
+      },
     "Resources" => {
       "myS3Bucket" => { "Type" => "AWS::S3::Bucket"},
       "EC2Instance" => {"Type" => "AWS::EC2::Instance",
@@ -19,22 +20,111 @@ tempHash = {
                           "SecurityGrops" => [{"Ref" => "InstanceSecurityGroup"}],
                           "SubnetId" => {"Ref" => "SubnetA"},
                           "KeyName" => {"Ref" => "KeyName"},
-                          "ImageId" => "ami-08935252a36e25f85"
+                          "ImageId" => latest_ami_id
                         }
       },
-      "InstanceSecurityGroup" => {"Type" => "AWS::EC2::SecurityGroup"},
-      "myVPC" => {"Type" => "AWS::EC2::VPC"},
+      "InstanceSecurityGroup" => {"Type" => "AWS::EC2::SecurityGroup",
+                                  "Properties" => {
+                                    "GroupDescription" => "Enable SSH access via port 22",
+                                    "SecurityGroupIngress" => [{
+                                      "IpProtocol" => "tcp",
+                                      "FromPort" => "22",
+                                      "ToPort" => "22",
+                                      "CidrIp" => allowed_ssh
+                                      }]
+                                  }
+      },
+      "myVPC" => {"Type" => "AWS::EC2::VPC",
+                  "Properties" => {
+                    "CidrBlock" => "10.0.0.0/16",
+                    "EnableDnsSupport" => "false",
+                    "EnableDnsHostnames" => "false",
+                    "InstanceTenancy" => "default"
+                  }
+      },
       "InternetGateway" => {"Type" => "AWS::EC2::InternetGateway"},
-      "VPCGatewayAttachment" => {"Type" => "AWS::EC2::VPCGatewayAttachment"},
-      "SubnetA" => {"Type" => "AWS::EC2::Subnet"},
-      "SubnetB" => {"Type" => "AWS::EC2::Subnet"},
-      "SubnetC" => {"Type" => "AWS::EC2::Subnet"},
-      "RouteTable" => {"Type" => "AWS::EC2::RouteTable"},
-      "InternetRoute" => {"Type" => "AWS::EC2::Route"},
-      "SubnetARouteTableAssociation" => {"Type" => "AWS::EC2::SubnetRouteTableAssociation"},
-      "SubnetBRouteTableAssociation" => {"Type" => "AWS::EC2::SubnetRouteTableAssociation"},
-      "SubnetCRouteTableAssociation" => {"Type" => "AWS::EC2::SubnetRouteTableAssociation"},
-      "SecurityGroup" => {"Type" => "AWS::EC2::SecurityGroup"}
+      "VPCGatewayAttachment" => {"Type" => "AWS::EC2::VPCGatewayAttachment",
+                                "Properties" => {
+                                   "VpcId" => { "Ref" => "myVPC" },
+                                   "InternetGatewayId" => { "Ref" => "InternetGateway" }
+                                }
+      },
+      "SubnetA" => {"Type" => "AWS::EC2::Subnet",
+                    "Properties" => {
+                       "AvailabilityZone" => "eu-west-1a",
+                       "VpcId" => { "Ref" => "myVPC" },
+                       "CidrBlock":"10.0.0.0/20",
+                       "MapPublicIpOnLaunch":true
+                    }
+      },
+      "SubnetB" => {"Type" => "AWS::EC2::Subnet",
+                    "Properties" => {
+                       "AvailabilityZone" => "eu-west-1b",
+                       "VpcId" => { "Ref" => "myVPC" },
+                       "CidrBlock":"10.0.16.0/20",
+                       "MapPublicIpOnLaunch":true
+                    }
+      },
+      "SubnetC" => {"Type" => "AWS::EC2::Subnet",
+                    "Properties" => {
+                       "AvailabilityZone" => "eu-west-1c",
+                       "VpcId" => { "Ref" => "myVPC" },
+                       "CidrBlock":"10.0.32.0/20",
+                       "MapPublicIpOnLaunch":true
+                    }
+      },
+      "RouteTable" => {"Type" => "AWS::EC2::RouteTable",
+                       "Properties" => {
+                          "VpcId" => { "Ref" => "myVPC" }
+                       }
+      },
+      "InternetRoute" => {"Type" => "AWS::EC2::Route",
+                          "DependsOn" => "VPCGatewayAttachment",
+                          "Properties" => {
+                             "DestinationCidrBlock" => "0.0.0.0/0",
+                             "GatewayId" => { "Ref" => "InternetGateway" },
+                             "RouteTableId" => { "Ref" => "RouteTable" }
+                          }
+      },
+      "SubnetARouteTableAssociation" => {"Type" => "AWS::EC2::SubnetRouteTableAssociation",
+                                        "Properties" => {
+                                           "RouteTableId" => { "Ref" => "RouteTable" },
+                                           "SubnetId" => { "Ref" => "SubnetA" }
+                                        }
+      },
+      "SubnetBRouteTableAssociation" => {"Type" => "AWS::EC2::SubnetRouteTableAssociation",
+                                        "Properties" => {
+                                           "RouteTableId" => { "Ref" => "RouteTable" },
+                                           "SubnetId" => { "Ref" => "SubnetA" }
+                                        }
+      },
+      "SubnetCRouteTableAssociation" => {"Type" => "AWS::EC2::SubnetRouteTableAssociation",
+                                        "Properties" => {
+                                           "RouteTableId" => { "Ref" => "RouteTable" },
+                                           "SubnetId" => { "Ref" => "SubnetA" }
+                                        }
+      },
+      "SecurityGroup" => {"Type" => "AWS::EC2::SecurityGroup",
+                          "Properties" => {
+                             "GroupName" => "Internet Group",
+                             "GroupDescription" => "SSH traffic in, all traffic out.",
+                             "VpcId" => { "Ref" => "myVPC" },
+                             "SecurityGroupIngress" => [
+                                {
+                                   "IpProtocol" => "tcp",
+                                   "FromPort" => "22",
+                                   "ToPort" => "22",
+                                   "CidrIp" => "35.158.238.101/32"
+                                }
+                             ],
+                             "SecurityGroupEgress" => [
+                                {
+                                   "IpProtocol" => -1,
+                                   "CidrIp" => "0.0.0.0/0"
+                                }
+                             ]
+                          }
+      }
   }
 }
 
